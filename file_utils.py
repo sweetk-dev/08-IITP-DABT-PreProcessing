5a1d8af57aaa280de0c70531a2f1fae0236e4b1c
sweetk-dev\dev1 <hhanlee@sweetk.co.kr>
2025-07-03 17:17:18 +0900

    [Change] 파일명에 포함할 수 없는 문자 치환해서 안전하게 저장하도록 수정

--- file_utils.py
+++ file_utils.py
@@ -2,13 +2,22 @@ import os
 import json
 from datetime import datetime
 import logging
+import re
 
 g_File_date_format = '%Y.%m.%d.%H%M%S'
 
+def safe_filename(filename, max_length=100):
+    # 파일명에 사용할 수 없는 문자 제거/치환
+    filename = re.sub(r'[\\/:*?"<>|]', '_', filename)
+    # 너무 긴 파일명은 잘라냄
+    if len(filename) > max_length:
+        filename = filename[:max_length]
+    return filename
+
 def save_data_and_meta_files(idx, data, meta, stats_src, folder):
     now = datetime.now()
-    stat_title = stats_src.get('stat_title', 'unknown')
-    stat_tbl_id = stats_src.get('stat_tbl_id', 'unknown')
+    stat_title = safe_filename(stats_src.get('stat_title', 'unknown'))
+    stat_tbl_id = safe_filename(str(stats_src.get('stat_tbl_id', 'unknown')))
     data_time = now.strftime(g_File_date_format)
     meta_time = now.strftime(g_File_date_format)
     data_filename = f"{idx}.data_{stat_title}_{stat_tbl_id}_{data_time}.json"
@@ -27,7 +36,9 @@ def save_data_and_meta_files(idx, data, meta, stats_src, folder):
 def save_meta_file(meta, stats_src, meta_dir, src_data_id, stat_title, from_year, to_year, file_format):
     now = datetime.now()
     time_str = now.strftime('%Y%m%d%H%M%S')
-    filename = f"meta_{src_data_id}-{stat_title}-{from_year}-{to_year}_{time_str}.{file_format}"
+    stat_title_safe = safe_filename(stat_title)
+    src_data_id_safe = safe_filename(str(src_data_id))
+    filename = f"meta_{src_data_id_safe}-{stat_title_safe}-{from_year}-{to_year}_{time_str}.{file_format}"
     meta_path = os.path.join(meta_dir, filename)
     with open(meta_path, 'w', encoding='utf-8') as f:
         if file_format == 'json':
@@ -39,7 +50,9 @@ def save_meta_file(meta, stats_src, meta_dir, src_data_id, stat_title, from_year
 def save_latest_file(latest, stats_src, latest_dir, src_data_id, stat_title, from_year, to_year, file_format):
     now = datetime.now()
     time_str = now.strftime('%Y%m%d%H%M%S')
-    filename = f"latest_{src_data_id}-{stat_title}-{from_year}-{to_year}_{time_str}.{file_format}"
+    stat_title_safe = safe_filename(stat_title)
+    src_data_id_safe = safe_filename(str(src_data_id))
+    filename = f"latest_{src_data_id_safe}-{stat_title_safe}-{from_year}-{to_year}_{time_str}.{file_format}"
     latest_path = os.path.join(latest_dir, filename)
     with open(latest_path, 'w', encoding='utf-8') as f:
         if file_format == 'json':
@@ -51,11 +64,14 @@ def save_latest_file(latest, stats_src, latest_dir, src_data_id, stat_title, fro
 def save_data_file(data, stats_src, data_dir, src_data_id, stat_title, from_str, to_str, file_format):
     now = datetime.now()
     time_str = now.strftime('%Y%m%d%H%M%S')
-    filename = f"data_{src_data_id}-{stat_title}-{from_str}-{to_str}_{time_str}.{file_format}"
+    stat_title_safe = safe_filename(stat_title)
+    src_data_id_safe = safe_filename(str(src_data_id))
+    filename = f"data_{src_data_id_safe}-{stat_title_safe}-{from_str}-{to_str}_{time_str}.{file_format}"
     data_path = os.path.join(data_dir, filename)
     with open(data_path, 'w', encoding='utf-8') as f:
         if file_format == 'json':
             json.dump(data, f, ensure_ascii=False, indent=2)
         else:
             f.write(str(data))
+    logging.debug(f"save_data_file: 파일 저장 완료 {data_path}")
     return data_path 
